6fd065fe8b517664842883403a94f3d18f73d366
animeshsingh7370 <animeshsingh161@gmail.com>
2023-12-23 20:58:13 +0530

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,95 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+ <div class="calculator">
+      <div class="profile">
+        <img class="menu-icon" src="@/assets/menu.png"  alt="">
+        <h3>Calculator</h3>
+        <img class="profile-img" src="@/assets/dalli-mask.png"  alt="">
+      </div>
+      <textarea  class="exp-box"  v-model="expValue" @input="validateInput" ></textarea>
+      <div class="button-box">
+          <button class="ac" @click="reset()">AC</button>
+          <button class="delete" @click="deleteLast()" >DEL</button>
+          <button class="modulus" @click="appendValue('%')">%</button>
+          <button class="divide" @click="appendValue('/')">/</button>
+          <button class="square_rt" @click="appendValue('3.14')">&Pi;</button>
+          <button class="seven" @click="appendValue('7')">7</button>
+          <button class="eight" @click="appendValue('8')">8</button>
+          <button class="nine" @click="appendValue('9')">9</button>
+          <button class="multiply" @click="appendValue('*')">*</button>
+          <button class="open_bra" @click="appendValue('(')">(</button>
+          <button class="four" @click="appendValue('4')">4</button>
+          <button class="five" @click="appendValue('5')">5</button>
+          <button class="six" @click="appendValue('6')">6</button>
+          <button class="minus" @click="appendValue('-')">-</button>
+          <button class="close_bra" @click="appendValue(')')">)</button>
+          <button class="one" @click="appendValue('1')">1</button>
+          <button class="two" @click="appendValue('2')">2</button>
+          <button class="three" @click="appendValue('3')">3</button>
+          <button class="plus" @click="appendValue('+')">+</button>
+          <button class="equal" @click="calculate()">=</button>
+          <button class="point" @click="appendValue('.')">.</button>
+          <button class="square" @click="appendValue('^')">^</button>
+          <button class="zero" @click="appendValue(0)">0</button>
+          
+        
+
+      </div>
+ </div>
 </template>
 
 <script>
+
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      expValue: '',
+      calcExp: ''
+      
+    };
+  },
+  methods: {
+    validateInput() {
+      // Allow only numbers and specific characters
+      this.expValue = this.expValue.replace(/[^0-9+\-*().]/g, '');
+    },
+    appendValue(value){
+      // console.log("ac");
+      if (this.expValue == 'Error'){
+        this.expValue = '';
+        this.expValue += value;
+      }else{
+        this.expValue += value;
+      }
+      
+    },
+    reset(){
+      this.expValue = '';
+    },
+    deleteLast(){
+      if (this.expValue == 'Error'){
+        this.expValue = '';
+      }this.expValue = this.expValue.slice(0, -1);
+      
+    },
+    calculate(){
+      if (this.expValue == 'Error'){
+        this.expValue = '';
+      }else{
+        
+        try {
+        this.calcExp = this.expValue.replace('^', '**')
+        this.calcExp = this.calcExp.replace('&Pi;', '3.14')
+        
+        this.expValue = String(eval(this.calcExp));
+      } catch (error) {
+        console.error(error);
+        this.expValue = 'Error';
+      }
+      }
+
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
